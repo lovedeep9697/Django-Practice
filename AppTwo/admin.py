c2ddb9be4311849536a6f35189c96dc5f2bb6e47
Lovedeep Singh <lovedeepsingh9697@gmail.com>
2018-04-03 23:16:14 +0530

    done

--- AppTwo/admin.py
+++ AppTwo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from AppTwo.models import Topic,Webpage
 # Register your models here.
+
+
+admin.site.register(Topic)
+admin.site.register(Webpage)
+
--- AppTwo/models.py
+++ AppTwo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+	topic_name = models.CharField(max_length = 264,unique = True)
+
+	def __str__(self):
+		return self.top_name
+
+class Webpage(models.Model):
+	topic = models.ForeignKey(Topic,on_delete = models.CASCADE)
+	name = models.CharField(max_length=264,unique = True)
+	url = models.URLField(unique =True)
