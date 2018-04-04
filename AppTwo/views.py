868af7d53b1cb4c3a0282776d9a6a991573f7ec8
Lovedeep Singh <lovedeepsingh9697@gmail.com>
2018-04-04 20:15:40 +0530

    population script

--- AppTwo/views.py
+++ AppTwo/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from AppTwo.models import Topic,Webpage
 
 # Create your views here.
 def help(request):
 	
-	my_dict = {'insert_me' : ' hii i am from views'}
+	webpage_name = Webpage.objects.all()
+	my_dict = {'names' : webpage_name}
+	# my_dict = {'insert_me' : ' hii i am from views'}
 	return render(request, 'AppTwo/index.html' , context = my_dict)
--- Django-Practice/settings.py
+++ Django-Practice/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'ProTwo.urls'
+ROOT_URLCONF = 'Django-Practice.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'ProTwo.wsgi.application'
+WSGI_APPLICATION = 'Django-Practice.wsgi.application'
 
 
 # Database
