70caf0b3cd81e09fca079ff6cb02a236f986e619
Lovedeep Singh <lovedeepsingh9697@gmail.com>
2018-04-08 01:32:39 +0530

    validator

--- AppTwo/forms.py
+++ AppTwo/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from django.core import validators
 
 class FormName(forms.Form):
 
 	Name = forms.CharField()
 	Email = forms.EmailField()
+	botcatcher = forms.CharField(required = False, widget= forms.HiddenInput, validators = [validators.MaxLengthValidator(0)])
